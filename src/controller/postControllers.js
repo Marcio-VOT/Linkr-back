1872b4a32a114f5a2d6e6e6295469b8d64a5ab5f
Marcio-VOT <marciofsantos65@gmail.com>
2023-03-10 03:39:48 -0300

    fix: repository getHashtag / controller getPost

--- src/controller/postControllers.js
+++ src/controller/postControllers.js
@@ -1,3 +1,4 @@
+import { getHashTags } from "../repositories/hashtagRepository.js";
 import {
   alterPostRepository,
   deletePostRepository,
@@ -5,31 +6,35 @@ import {
   registerPostRepository,
 } from "../repositories/postRepository.js";
 
-export async function registerPost(req, res){
-    const {description, externalLink, hashtags} = req.body;
-    const userId = res.locals.userId;
+export async function registerPost(req, res) {
+  const { description, externalLink, hashtags } = req.body;
+  const userId = res.locals.userId;
 
-    try {
-        const post_id = await registerPostRepository(userId, description, externalLink);
-        if(!hashtags) res.send("Post criado").status(201);
-        else{
-            hashtags.map(e => insertHashtagOnDb(e, post_id)) 
-            res.send("Post criado").status(201)
-        }
-    } catch (error) {
-        res.status(500).send(error.message);
+  try {
+    const post_id = await registerPostRepository(
+      userId,
+      description,
+      externalLink
+    );
+    if (!hashtags) res.send("Post criado").status(201);
+    else {
+      hashtags.map((e) => insertHashtagOnDb(e, post_id));
+      res.send("Post criado").status(201);
     }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
 
-export async function getPosts(req, res){
-    try {
-        const resultPost = await getPostsRepository();
-        const resultHashtags = await getHashTags()
-        res.send({posts: resultPost.rows, hashtags: resultHashtags});
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send(error.message);
-    }
+export async function getPosts(req, res) {
+  try {
+    const resultPost = await getPostsRepository();
+    const resultHashtags = await getHashTags();
+    res.send({ posts: resultPost.rows, hashtags: resultHashtags });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(error.message);
+  }
 }
 
 export async function deletePost(req, res) {
--- src/repositories/hashtagRepository.js
+++ src/repositories/hashtagRepository.js
@@ -1,30 +1,41 @@
 import db from "../config/db.js";
 
-export const insertHashtagOnDb = async(hashtag, post_id) => {
-    const result = await db.query(`INSERT INTO hashtags
-    (hashtag) values ($1) RETURNING id`, [hashtag])
-    const hashtag_id = result.rows[0].id
-    await db.query(`INSERT INTO post_hashtags 
-    (hashtag_id, post_id) values ($1, $2)`, [hashtag_id, post_id])
-}
+export const insertHashtagOnDb = async (hashtag, post_id) => {
+  const result = await db.query(
+    `INSERT INTO hashtags
+    (hashtag) values ($1) RETURNING id`,
+    [hashtag]
+  );
+  const hashtag_id = result.rows[0].id;
+  await db.query(
+    `INSERT INTO post_hashtags 
+    (hashtag_id, post_id) values ($1, $2)`,
+    [hashtag_id, post_id]
+  );
+};
 
-export const getHashTags = async() => {
-    const fetchHashtags =  await db.query(`SELECT hashtag from hashtags`)
-    console.log(fetchHashtags.rows)
-    return fetchHashtags.rows
-}
+export const getHashTags = async () => {
+  const fetchHashtags = await db.query(`SELECT hashtags from hashtags`);
+  console.log(fetchHashtags.rows);
+  return fetchHashtags.rows;
+};
 
-export const getPostHashTags = async(hashtag) => {
-    const postHashtagsIds = await db.query(`SELECT post_id from hashtags 
-    WHERE hashtag = $1`, [hashtag])
-    const posts = postHashtagsIds.rows.map(async e =>  {
-      return await db.query(`
+export const getPostHashTags = async (hashtag) => {
+  const postHashtagsIds = await db.query(
+    `SELECT post_id from hashtags 
+    WHERE hashtag = $1`,
+    [hashtag]
+  );
+  const posts = postHashtagsIds.rows.map(async (e) => {
+    return await db.query(
+      `
       SELECT p.description, p.external_link, p.publish_date, u.name, u.profile_picture 
       FROM posts p 
       JOIN users u 
       ON p.user_id = u.id
-      WHERE p."id" = $1`, [e.post_id]);
-})
-  return posts.rows
-}
-
+      WHERE p."id" = $1`,
+      [e.post_id]
+    );
+  });
+  return posts.rows;
+};
