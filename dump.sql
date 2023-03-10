fb19bb3eb21bc46afc1109bfa883fe45668f5879
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-03-10 20:49:30 -0300

    add dump

--- dump.sql
+++ dump.sql
@@ -26,7 +26,7 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.hashtags (
     id integer NOT NULL,
-    hashtags text
+    hashtag text NOT NULL
 );
 
 
@@ -121,7 +121,8 @@ CREATE TABLE public.posts (
     id integer NOT NULL,
     user_id integer NOT NULL,
     description text NOT NULL,
-    external_link text NOT NULL
+    external_link text NOT NULL,
+    publish_date timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
@@ -217,6 +218,7 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: hashtags; Type: TABLE DATA; Schema: public; Owner: -
 --
 
+INSERT INTO public.hashtags VALUES (5, '#express');
 
 
 --
@@ -229,12 +231,32 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: post_hashtags; Type: TABLE DATA; Schema: public; Owner: -
 --
 
+INSERT INTO public.post_hashtags VALUES (85, 70, 5);
 
 
 --
 -- Data for Name: posts; Type: TABLE DATA; Schema: public; Owner: -
 --
 
+INSERT INTO public.posts VALUES (56, 1, 'um teste 1', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 17:16:09.102');
+INSERT INTO public.posts VALUES (57, 1, 'um teste aleatorio', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 17:30:10.885');
+INSERT INTO public.posts VALUES (58, 1, 'teste 12', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 17:31:27.636');
+INSERT INTO public.posts VALUES (59, 1, 'Teste 13', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 17:42:59.538');
+INSERT INTO public.posts VALUES (60, 1, 'Testando', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 18:10:25.443');
+INSERT INTO public.posts VALUES (61, 1, 'teste 15', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 18:20:33.861');
+INSERT INTO public.posts VALUES (62, 1, 'teste 16 #postgres', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:21:01.234');
+INSERT INTO public.posts VALUES (63, 1, 'teste 123', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:33:14.937');
+INSERT INTO public.posts VALUES (64, 1, 'teste 123 #linkedin', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:33:32.23');
+INSERT INTO public.posts VALUES (65, 1, 'teste 124', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:38:53.02');
+INSERT INTO public.posts VALUES (66, 1, '123456', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:40:07.353');
+INSERT INTO public.posts VALUES (67, 1, 'teste 12345', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:42:17.277');
+INSERT INTO public.posts VALUES (68, 1, 'teste 12345', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:43:04.711');
+INSERT INTO public.posts VALUES (69, 1, 'teste 24', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:44:27.913');
+INSERT INTO public.posts VALUES (70, 1, 'teste #express', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:46:31.283');
+INSERT INTO public.posts VALUES (71, 1, '12345 teste', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:49:59.767');
+INSERT INTO public.posts VALUES (72, 1, 'testando', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:58:29.324');
+INSERT INTO public.posts VALUES (73, 1, 'testando 2', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:59:11.725');
+INSERT INTO public.posts VALUES (74, 1, 'testando 2', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:59:32.658');
 
 
 --
@@ -246,41 +268,43 @@ INSERT INTO public.users VALUES (5, 'Dennis', 'dennispinheiro97@gmail.com', '$2b
 INSERT INTO public.users VALUES (6, 'jane', 'jane@acme.com', '$2b$06$A22QkC8Yl5ZsujGQ20Cafe3UhClpSfrzfwclzUHFnMsoTT15aIZGK', 'https://avatars.githubusercontent.com/u/62717698?v=4');
 INSERT INTO public.users VALUES (7, 'aadad', 'aaadad@ad.com', '$2b$06$ElTSiTj.KHFLP6jlN5GyfOxzhexliUgastjlw4YX4ds/PvJLNNS7O', 'https://avatars.githubusercontent.com/u/62717698?v=4');
 INSERT INTO public.users VALUES (8, 'Dickson', 'dicksonpinheiro97@gmail.com', '$2b$06$l1pSGLprfaOfiXklJfmYyehIukvOXxhzA4z6QDLXNKPkUJInhTIHi', 'https://avatars.githubusercontent.com/u/62717698?v=4');
+INSERT INTO public.users VALUES (9, 'Dickson 2', 'dicksonpinheiro45@gmail.com', '$2b$06$3mQNcbSILbMR9XJVVNC6DuEmK05EEv8OGOBm4jRZIuLI8qPdh6wpq', 'https://avatars.githubusercontent.com/u/62717698?v=4');
+INSERT INTO public.users VALUES (10, 'Den', 'dennis@gmail.com', '$2b$06$Uma7osW6fSSVnYRzvtqxde.CDIayyJeguVU/QI80//.mMaa5ZmDcu', 'https://upload.wikimedia.org/wikipedia/commons/7/7d/Caranguejo_u%C3%A7%C3%A1.png');
 
 
 --
 -- Name: hashtags_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.hashtags_id_seq', 1, false);
+SELECT pg_catalog.setval('public.hashtags_id_seq', 5, true);
 
 
 --
 -- Name: likes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.likes_id_seq', 1, false);
+SELECT pg_catalog.setval('public.likes_id_seq', 8, true);
 
 
 --
 -- Name: post_hashtags_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.post_hashtags_id_seq', 1, false);
+SELECT pg_catalog.setval('public.post_hashtags_id_seq', 85, true);
 
 
 --
 -- Name: posts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.posts_id_seq', 1, false);
+SELECT pg_catalog.setval('public.posts_id_seq', 74, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 8, true);
+SELECT pg_catalog.setval('public.users_id_seq', 10, true);
 
 
 --
