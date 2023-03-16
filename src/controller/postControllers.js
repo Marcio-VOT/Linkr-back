0013e16590ce508ebf3feb630c97519ec85147be
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-03-16 20:57:49 -0300

    fix: tratando situação em que o status 201 não era enviado e requisição ficava sem resposta

--- src/controller/postControllers.js
+++ src/controller/postControllers.js
@@ -1,47 +1,34 @@
-import { getHashTags, insertHashtagOnDb, insertPostHashtag, getTagByPostId, getTagByName } from "../repositories/hashtagRepository.js";
+import { getHashTags, insertHashtagOnDb, insertPostHashtag, getTagByName } from "../repositories/hashtagRepository.js";
 import { alterPostRepository, deletePostRepository, getPostsRepository, registerPostRepository } from "../repositories/postRepository.js";
 
 export async function registerPost(req, res) {
   let { description, externalLink, hashtags } = req.body;
-  const userId = res.locals.userId;
-  const tagRows = [];
+const userId = res.locals.userId;
+const tagRows = [];
 
-  try {
-    const postId = await registerPostRepository(userId, description, externalLink);
-    if (hashtags.length === 0) {
-      return res.sendStatus(201);
-    }
+try {
+  const postId = await registerPostRepository(userId, description, externalLink);
 
-    hashtags.forEach(async (e, i) => {
-      const resultHashtag = await getTagByName(e)
-      if (resultHashtag[0]) {
-        if (tagRows[0]) {
-          console.log(tagRows.includes(e))
-          if (tagRows.includes(e)) {
-            if (hashtags.length - 1 === i) {
-              console.log(tagRows)
-              console.log("------")
-              return res.sendStatus(201)
-            }
-            return;
-          }
-        }
-        tagRows.push(e)
-        await insertPostHashtag(resultHashtag[0].id, postId)
-        if (hashtags.length - 1 === i) {
-          return res.sendStatus(201)
-        }
-        return
+  for (let i = 0; i < hashtags.length; i++) {
+    const e = hashtags[i];
+    const resultHashtag = await getTagByName(e);
+    if (resultHashtag[0]) {
+      if (!tagRows.includes(e)) {
+        tagRows.push(e);
+        await insertPostHashtag(resultHashtag[0].id, postId);
       }
-      const resultInsertHashtag = await insertHashtagOnDb(e)
-      tagRows.push(e)
-      await insertPostHashtag(resultInsertHashtag.rows[0].id, postId)
-      res.sendStatus(201)
-    })
-  } catch (error) {
-    console.log(error)
-    res.status(500).send(error.message);
+    } else {
+      const resultInsertHashtag = await insertHashtagOnDb(e);
+      tagRows.push(e);
+      await insertPostHashtag(resultInsertHashtag.rows[0].id, postId);
+    }
   }
+
+  res.sendStatus(201);
+} catch (error) {
+  console.log(error);
+  res.status(500).send(error.message);
+}
 }
 
 export async function getPosts(req, res) {
