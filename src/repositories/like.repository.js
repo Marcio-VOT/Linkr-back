ea2d43d4fe1f847aa0529bb4e227480333dac19d
andreprogramador01 <andreprogramador01@gmail.com>
2023-03-21 16:58:17 -0300

    feat:like and unlike

--- src/repositories/like.repository.js
+++ src/repositories/like.repository.js
@@ -1,8 +1,8 @@
 import db from "../config/db.js";
 
-export async function countLikes(idUser,idPost){
+export async function countLikes(idUser, idPost) {
     try {
-        const {rows:countLike } = await db.query(`SELECT count(*) FROM likes WHERE post_id=$1 AND status=true`, [idPost])
+        const { rows: countLike } = await db.query(`SELECT count(*) FROM likes WHERE post_id=$1`, [idPost])
 
         return countLike[0].count
     } catch (error) {
@@ -10,30 +10,39 @@ export async function countLikes(idUser,idPost){
 
     }
 }
-export async function newLike(idUser,idPost){
+export async function newLike(idUser, idPost) {
     try {
-        await db.query(`INSERT INTO likes (user_id,post_id) VALUES($1,$2)`,[idUser,idPost])
+        await db.query(`INSERT INTO likes (user_id,post_id, status) VALUES($1,$2, $3)`, [idUser, idPost, false])
     } catch (error) {
         console.log(error)
     }
 
 }
-export async function unlike(idUser,idPost){
-    
+export async function unlike(idUser, idPost) {
+
     try {
-        await db.query(`DELETE FROM likes WHERE user_id=$1 AND post_id=$2`,[idUser,idPost])
-        
+        await db.query(`DELETE FROM likes WHERE user_id=$1 AND post_id=$2`, [idUser, idPost])
+
     } catch (error) {
         console.log(error)
-        
+
     }
 }
-export async function twoUsers(idUser){
+export async function twoUsers(idPost) {
     try {
-        const result = await db.query(`SELECT name FROM users JOIN likes ON users.id = likes.user_id LIMIT 2`)
+        const result = await db.query(`SELECT name FROM users JOIN likes ON users.id = likes.user_id WHERE likes.post_id=$1 LIMIT 2`, [idPost])
         return result.rows
     } catch (error) {
-        console.log(error) 
+        console.log(error)
+    }
+
+}
+export async function youLike(idUser, idPost) {
+    try {
+        const result = await db.query(`SELECT * FROM likes WHERE user_id=$1 AND post_id=$2 limit 1`, [idUser, idPost])
+        return result.rows[0]?.status ? true : result.rows[0]?.status
+    } catch (error) {
+        console.log(error)
     }
 
 }
