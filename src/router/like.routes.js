4c021513a28e9b35ec330adda0f4f5bb23062823
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-03-24 17:05:30 -0300

    likeRoutesValidation

--- src/router/like.routes.js
+++ src/router/like.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { getLikes, removeLike, likeByPost, getTwoUsers, getYouLike} from "../controller/likeController.js";
+import validateToken from "../middlewares/validateToken.js"
 
 const likeRouter = Router()
 
-likeRouter.post('/getLikes', getLikes)
-likeRouter.post('/newLike',likeByPost)
-likeRouter.post('/removeLike',removeLike)
-likeRouter.post('/twoUsers',getTwoUsers)
-likeRouter.post('/youLike',getYouLike)
+likeRouter.post('/getLikes', validateToken, getLikes)
+likeRouter.post('/newLike', validateToken, likeByPost)
+likeRouter.post('/removeLike', validateToken, removeLike)
+likeRouter.post('/twoUsers', validateToken,getTwoUsers)
+likeRouter.post('/youLike', validateToken, getYouLike)
 
 export default likeRouter
