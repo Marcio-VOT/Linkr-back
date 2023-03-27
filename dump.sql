701b50ecf977196f24b5f482d85cd5e23141ff75
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-03-27 00:49:56 -0300

    atualizando dump

--- dump.sql
+++ dump.sql
@@ -21,12 +21,75 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: comments; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.comments (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    post_id integer NOT NULL,
+    comment text NOT NULL
+);
+
+
+--
+-- Name: comments_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.comments_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: comments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.comments_id_seq OWNED BY public.comments.id;
+
+
+--
+-- Name: follow; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.follow (
+    id integer NOT NULL,
+    follower_id integer NOT NULL,
+    user_id integer NOT NULL
+);
+
+
+--
+-- Name: follow_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.follow_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: follow_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.follow_id_seq OWNED BY public.follow.id;
+
+
+--
 -- Name: hashtags; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.hashtags (
     id integer NOT NULL,
-    hashtag text NOT NULL
+    hashtags text NOT NULL
 );
 
 
@@ -147,6 +210,38 @@ ALTER SEQUENCE public.posts_id_seq OWNED BY public.posts.id;
 
 
 --
+-- Name: repost; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.repost (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    post_id integer NOT NULL,
+    publish_date timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+--
+-- Name: repost_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.repost_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: repost_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.repost_id_seq OWNED BY public.repost.id;
+
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -180,6 +275,20 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: comments id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments ALTER COLUMN id SET DEFAULT nextval('public.comments_id_seq'::regclass);
+
+
+--
+-- Name: follow id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.follow ALTER COLUMN id SET DEFAULT nextval('public.follow_id_seq'::regclass);
+
+
+--
 -- Name: hashtags id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -208,6 +317,13 @@ ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_
 
 
 --
+-- Name: repost id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.repost ALTER COLUMN id SET DEFAULT nextval('public.repost_id_seq'::regclass);
+
+
+--
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -215,10 +331,21 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
+-- Data for Name: comments; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: follow; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
 -- Data for Name: hashtags; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.hashtags VALUES (5, '#express');
 
 
 --
@@ -231,80 +358,96 @@ INSERT INTO public.hashtags VALUES (5, '#express');
 -- Data for Name: post_hashtags; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.post_hashtags VALUES (85, 70, 5);
 
 
 --
 -- Data for Name: posts; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.posts VALUES (56, 1, 'um teste 1', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 17:16:09.102');
-INSERT INTO public.posts VALUES (57, 1, 'um teste aleatorio', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 17:30:10.885');
-INSERT INTO public.posts VALUES (58, 1, 'teste 12', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 17:31:27.636');
-INSERT INTO public.posts VALUES (59, 1, 'Teste 13', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 17:42:59.538');
-INSERT INTO public.posts VALUES (60, 1, 'Testando', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 18:10:25.443');
-INSERT INTO public.posts VALUES (61, 1, 'teste 15', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 18:20:33.861');
-INSERT INTO public.posts VALUES (62, 1, 'teste 16 #postgres', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:21:01.234');
-INSERT INTO public.posts VALUES (63, 1, 'teste 123', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:33:14.937');
-INSERT INTO public.posts VALUES (64, 1, 'teste 123 #linkedin', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:33:32.23');
-INSERT INTO public.posts VALUES (65, 1, 'teste 124', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:38:53.02');
-INSERT INTO public.posts VALUES (66, 1, '123456', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:40:07.353');
-INSERT INTO public.posts VALUES (67, 1, 'teste 12345', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:42:17.277');
-INSERT INTO public.posts VALUES (68, 1, 'teste 12345', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:43:04.711');
-INSERT INTO public.posts VALUES (69, 1, 'teste 24', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:44:27.913');
-INSERT INTO public.posts VALUES (70, 1, 'teste #express', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:46:31.283');
-INSERT INTO public.posts VALUES (71, 1, '12345 teste', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:49:59.767');
-INSERT INTO public.posts VALUES (72, 1, 'testando', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:58:29.324');
-INSERT INTO public.posts VALUES (73, 1, 'testando 2', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:59:11.725');
-INSERT INTO public.posts VALUES (74, 1, 'testando 2', 'https://github.com/Marcio-VOT/Linkr-back/', '2023-03-10 20:59:32.658');
+
+
+--
+-- Data for Name: repost; Type: TABLE DATA; Schema: public; Owner: -
+--
+
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.users VALUES (1, 'Dickson', 'dicksonpinheiro4@gmail.com', '$2b$06$S.qttR4DeFehejY9pjclfu0ncgii6rVrA4fAxyXlfN/rSK5ysv88C', 'https://avatars.githubusercontent.com/u/62717698?v=4');
-INSERT INTO public.users VALUES (5, 'Dennis', 'dennispinheiro97@gmail.com', '$2b$06$ADHxSRr9lV4BFqxV3EFmxOLbgxY4pxlZh2VJeKfbL2.mxbp6RiX5a', 'https://avatars.githubusercontent.com/u/62717698?v=4');
-INSERT INTO public.users VALUES (6, 'jane', 'jane@acme.com', '$2b$06$A22QkC8Yl5ZsujGQ20Cafe3UhClpSfrzfwclzUHFnMsoTT15aIZGK', 'https://avatars.githubusercontent.com/u/62717698?v=4');
-INSERT INTO public.users VALUES (7, 'aadad', 'aaadad@ad.com', '$2b$06$ElTSiTj.KHFLP6jlN5GyfOxzhexliUgastjlw4YX4ds/PvJLNNS7O', 'https://avatars.githubusercontent.com/u/62717698?v=4');
-INSERT INTO public.users VALUES (8, 'Dickson', 'dicksonpinheiro97@gmail.com', '$2b$06$l1pSGLprfaOfiXklJfmYyehIukvOXxhzA4z6QDLXNKPkUJInhTIHi', 'https://avatars.githubusercontent.com/u/62717698?v=4');
-INSERT INTO public.users VALUES (9, 'Dickson 2', 'dicksonpinheiro45@gmail.com', '$2b$06$3mQNcbSILbMR9XJVVNC6DuEmK05EEv8OGOBm4jRZIuLI8qPdh6wpq', 'https://avatars.githubusercontent.com/u/62717698?v=4');
-INSERT INTO public.users VALUES (10, 'Den', 'dennis@gmail.com', '$2b$06$Uma7osW6fSSVnYRzvtqxde.CDIayyJeguVU/QI80//.mMaa5ZmDcu', 'https://upload.wikimedia.org/wikipedia/commons/7/7d/Caranguejo_u%C3%A7%C3%A1.png');
+
+
+--
+-- Name: comments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.comments_id_seq', 10, true);
+
+
+--
+-- Name: follow_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.follow_id_seq', 61, true);
 
 
 --
 -- Name: hashtags_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.hashtags_id_seq', 5, true);
+SELECT pg_catalog.setval('public.hashtags_id_seq', 7, true);
 
 
 --
 -- Name: likes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.likes_id_seq', 8, true);
+SELECT pg_catalog.setval('public.likes_id_seq', 47, true);
 
 
 --
 -- Name: post_hashtags_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.post_hashtags_id_seq', 85, true);
+SELECT pg_catalog.setval('public.post_hashtags_id_seq', 21, true);
 
 
 --
 -- Name: posts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.posts_id_seq', 74, true);
+SELECT pg_catalog.setval('public.posts_id_seq', 31, true);
+
+
+--
+-- Name: repost_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.repost_id_seq', 1, false);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 10, true);
+SELECT pg_catalog.setval('public.users_id_seq', 5, true);
+
+
+--
+-- Name: comments comments_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: follow follow_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.follow
+    ADD CONSTRAINT follow_pkey PRIMARY KEY (id);
 
 
 --
@@ -340,6 +483,14 @@ ALTER TABLE ONLY public.posts
 
 
 --
+-- Name: repost repost_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.repost
+    ADD CONSTRAINT repost_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -353,6 +504,38 @@ ALTER TABLE ONLY public.users
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: comments comments_post_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_post_id_fkey FOREIGN KEY (post_id) REFERENCES public.posts(id);
+
+
+--
+-- Name: comments comments_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: follow follow_follower_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.follow
+    ADD CONSTRAINT follow_follower_id_fkey FOREIGN KEY (follower_id) REFERENCES public.users(id);
+
+
+--
+-- Name: follow follow_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.follow
+    ADD CONSTRAINT follow_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
 
 
 --
@@ -396,5 +579,22 @@ ALTER TABLE ONLY public.posts
 
 
 --
+-- Name: repost repost_post_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.repost
+    ADD CONSTRAINT repost_post_id_fkey FOREIGN KEY (post_id) REFERENCES public.posts(id);
+
+
+--
+-- Name: repost repost_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.repost
+    ADD CONSTRAINT repost_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
 -- PostgreSQL database dump complete
 --
+
